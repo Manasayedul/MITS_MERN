75781ce71879b677405b5d5308968d79ae1fc019
Manasa <manasayeddula25@gmail.com>
2026-01-29 12:01:23 +0530

    Frontend added

--- Frontend/src/pages/Contact.jsx
+++ Frontend/src/pages/Contact.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import { useState } from 'react'
+import Service from './Service';
 
-const Contact = (props) => {
+const Contact = () => {
+    const [name,setName]=useState("Manasa");
+    const toggleName=()=>{
+        setName(name==="Manasa"?"Manu":"Manasa")
+    }
   return (
-    <div>Contact :{props.Contact}</div>
+    <div>
+        <h1>Name : {name}</h1>
+        <button onClick={toggleName}>Toggle</button>
+        
+    </div>
   )
 }
 
