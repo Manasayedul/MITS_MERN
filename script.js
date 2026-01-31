4750d31242ea9a90a057be6bce1e176e3da8d422
Manasa <manasayeddula25@gmail.com>
2026-01-31 11:40:53 +0530

    first commit

--- script.js
+++ script.js
@@ -168,26 +168,146 @@
 // console.log(z);
 // var z;
 
-// Spread operator (...)
-var arr1=[10,20,30];
-var arr2=[...arr1,40,50,60];
-console.log(arr2);
+// // Spread operator (...)
+// var arr1=[10,20,30];
+// var arr2=[...arr1,40,50,60];
+// console.log(arr2);
 
-//Destructing operator
-var [m1,m2,m3,m4,m5]=[89,90,99,96,87]
-console.log(m1);
-console.log(m2);
-console.log(m3);
-console.log(m4);
-console.log(m5);
+// //Destructing operator
+// var [m1,m2,m3,m4,m5]=[89,90,99,96,87]
+// console.log(m1);
+// console.log(m2);
+// console.log(m3);
+// console.log(m4);
+// console.log(m5);
 
-var {name,age,Dept,marks}={
-    name:"Manu",
-    age:20,
-    Dept:"DS",
-    marks:100
+// var {name,age,Dept,marks}={
+//     name:"Manu",
+//     age:20,
+//     Dept:"DS",
+//     marks:100
+// }
+// console.log(name);
+// console.log(age);
+// console.log(Dept);
+// console.log(marks);
+
+// let arr=[10,20,30,40];
+// //Syntax: for (variable variable name in array_name){
+// // statements
+// //}
+// for(let i in arr){
+//     console.log(i,arr[i]);
+// }
+
+// var obj={
+//     name:"Manu",
+//     age:20
+// }
+// for (let i in obj){
+//     console.log(i,obj[i]);
+// }
+
+// //Syntax: for (variable variable_name of array_name){
+// //statements
+// //}
+
+// for(let val of arr){
+//     console.log(val);
+// }
+
+// let arr =[1,2,3,4];
+// //map
+// var double_arr=arr.map((i)=>(i*2));
+// console.log(double_arr);
+
+// //filter
+// var even=arr.filter((i)=>(i%2===0))
+// console.log(even);
+
+// //reduce
+// var total =arr.reduce((sum,i)=>(sum+i),0)
+// console.log(total);
+
+// var total_sum=arr.map((num)=>num*2).filter((num)=>(num%2===0)).reduce((sum,num)=>(sum+num),0)
+// console.log(total_sum)
+
+// var n=7;
+// var flag=true;
+// if(n<=1) flag = false;
+// else{
+//     for (let i=2;i<=n/2;i++){
+//         if(n%i===0){
+//             flag=false;
+//             break;
+
+//         }
+//     }
+// }
+// console.log(flag?"Prime":"not Prime");
+
+// var name="Manasa";
+// console.log('My name is ${name}');
+
+// Array.forEach((value,index)=>{
+//     console.log(index,value);
+// })
+
+// var add=(a,b,callBack)=>{
+//     var result=a+b;
+//     callBack(result);
+// }
+// add(10,20,(res)=>{console.log(res)})
+
+// var main=(callBack)=>{
+//     console.log("I am in main")
+//     callBack()
+// }
+// var demo=()=>{
+//     console.log("I am callBack");
+// }
+// main(demo)
+
+// var promise=new Promise((resolve,reject)=>{
+//     var success=true;
+//     if(success){
+//         resolve("Promise Resolved");
+
+//     }
+//     else{
+//         reject("Promise Rejected");
+//     }
+// })
+// promise.then((res)=>console.log(res)).catch((err)=>console.log(err))
+
+// var promise=new Promise((resolve,reject)=>{
+//     var success=true;
+//     if(success){
+//         setTimeout(()=>{
+//             resolve("Promise Resolved")
+//         },5000)
+
+//     }
+//     else{
+//         reject("Promise Rejected");
+//     }
+// })
+// promise.then((res)=>console.log(res)).catch((err)=>console.log(err))
+
+// const getData=()=>{
+//     return fetch('https://jsonplaceholder.typicode.com/posts')
+// }
+// getData().then((res)=>res.json())
+// .then((data)=>console.log(data))
+// .catch(err=>console.log(err))
+
+const getData=async()=>{
+    try{
+        var res=await fetch("https://jsonplaceholder.typicode.com/posts")
+        var data=await res.json();
+        console.log(data);
+    }catch(err){
+        console.log(err);
+    }
 }
-console.log(name);
-console.log(age);
-console.log(Dept);
-console.log(marks);
+getData();    
